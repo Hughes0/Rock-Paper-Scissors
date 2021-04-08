309896257369424c82f03a6f16839b8381ebdbab
Hughes <codedpiano@gmail.com>
2021-04-08 14:41:19 -0400

    rock paper scissors game

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-const selectionOptions = ["Rock", "Paper", "Scissors"]
+const selectionOptions = ["rock", "paper", "scissors"];
 
-function computerPlay(playerSelection, computerSelection) {
+const rules = {
+    rock: {
+        rock: "tie",
+        paper: "lose",
+        scissors: "win"
+    },
+    paper: {
+        rock: "win",
+        paper: "tie",
+        scissors: "lose"
+    },
+    scissors: {
+        rock: "lose",
+        paper: "win",
+        scissors: "tie"
+    }
+};
+
+
+function computerPlay() {
     return selectionOptions[Math.floor(Math.random() * 3)];
+};
+
+
+function playRound(playerSelection, computerSelection) {
+    let result = rules[playerSelection.toLowerCase()][computerSelection.toLowerCase()];
+    return result;
+};
+
+
+function game() {
+    let wins = 0;
+    let ties = 0;
+    let losses = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Please enter a selection: ");
+        let result = playRound(playerSelection, computerPlay());
+        if (result == "win") {
+            wins++;
+        };
+        if (result == "tie") {
+            ties++;
+        };
+        if (result == "lose") {
+            losses++;
+        };
+    };
+    if (wins > losses) {
+        console.log("You win");
+    } else if (losses > wins) {
+        console.log("You lose");
+    } else {
+        console.log("You tied");
+    };
 };
